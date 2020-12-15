d01703b965e42e30fb1f39cb384233eca73f4d2c
rolfboom <rolf.boomgaarden@googlemail.com>
2020-12-15 09:40:27 +0100

    day 15 part 2

--- 2020/15/script.mjs
+++ 2020/15/script.mjs
@@ -17,24 +17,17 @@ const part1 = (data, rounds = 2020) => {
 };
 
 const part2 = (data, rounds = 2020) => {
-    const numbers = data.reduce(
-        (numbers, num, index) => ({ ...numbers, [num]: { last: index + 1 } }),
-        {}
-    );
-    // const numbers = data.reduce((numbers, num, index) => {
-    //     numbers.set(num, [index + 1]);
-    //     return numbers;
-    // }, new Map());
+    const numbers = data.reduce((numbers, num, index) => {
+        numbers.set(num, [index + 1]);
+        return numbers;
+    }, new Map());
     let number = data[data.length - 1];
 
     for (let i = data.length + 1; i <= rounds; i += 1) {
-        number = !numbers[number].before
-            ? 0
-            : numbers[number].last - numbers[number].before;
-        numbers[number] = {
-            before: numbers[number] && numbers[number].last,
-            last: i,
-        };
+        const round = numbers.get(number);
+        number = round.length <= 1 ? 0 : round[0] - round[1];
+        const newNum = numbers.get(number);
+        numbers.set(number, [i, ...(newNum ? [newNum[0]] : [])]);
     }
     return number;
 };
@@ -43,12 +36,11 @@ assert(part1(parse(testInput)) === 436);
 console.log('answer one:', part1(parse(input)));
 
 assert(part2(parse(testInput)) === 436);
-console.log(part2([0, 3, 6], 3000000));
-// assert(part2([0, 3, 6], 30000000) === 175594);
-// assert(part2([1, 3, 2], 30000000) === 2578);
-// assert(part2([2, 1, 3], 30000000) === 3544142);
-// assert(part2([1, 2, 3], 30000000) === 261214);
-// assert(part2([2, 3, 1], 30000000) === 6895259);
-// assert(part2([3, 2, 1], 30000000) === 18);
-// assert(part2([3, 1, 2], 30000000) === 362);
-// console.log('answer two:', part2(parse(input), 30000000));
+assert(part2([0, 3, 6], 30000000) === 175594);
+assert(part2([1, 3, 2], 30000000) === 2578);
+assert(part2([2, 1, 3], 30000000) === 3544142);
+assert(part2([1, 2, 3], 30000000) === 261214);
+assert(part2([2, 3, 1], 30000000) === 6895259);
+assert(part2([3, 2, 1], 30000000) === 18);
+assert(part2([3, 1, 2], 30000000) === 362);
+console.log('answer two:', part2(parse(input), 30000000));
